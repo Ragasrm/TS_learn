7538f7cc5ddb59b3ccb128115470445a7da8ee3c
ragavsky <external.ragavendiran.saravanan@sky.de>
2024-08-12 08:22:24 +0530

    [CLASS] Basic class setup in JS

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,2 +1,12 @@
-"use strict";
-console.log("Hello Class TS");
+class Person{
+
+taunt(){
+    console.log("BOOOOO")
+}
+
+}
+
+
+const player1 = new Person()
+player1.taunt()
+console.log("player1", player1)
